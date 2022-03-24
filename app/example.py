fc6748dd304d4de772488e3eca11196c98fb61f1
George-MF <72128377+George-MF@users.noreply.github.com>
2022-03-24 21:44:37 +0000

    Create example.py

---
+++ app/example.py
@@ -0,0 +1,60 @@
+"""
+Example program with doctests
+"""
+
+
+def add(a, b):
+    """
+    >>> add(1, 2)
+    3
+    >>> add(1, -2)
+    -1
+    >>> add(1000000000000000000, 2)
+    1000000000000000002
+    """
+    return a + b
+
+
+def multiply(a, b):
+    """
+    >>> multiply(1, 2)
+    2
+    >>> multiply(2, 3)
+    6
+    """
+    return a * b
+
+
+def combine(a, b):
+    """
+    >>> combine(1, 2)
+    6
+    >>> combine(0, 1)
+    -2
+    >>> combine(2, 3)
+    22
+    """
+    return (
+        multiply(
+            add(a, b),
+            add(b, a),
+        )
+        - 3
+    )
+
+
+if __name__ == "__main__":
+    import random
+
+    numbers = tuple(
+        combine(
+            random.randint(0, 100),
+            random.randint(0, 100),
+        )
+        for i in range(10)
+    )
+
+    import json
+
+    data = json.dumps(numbers)
+    print(data)
